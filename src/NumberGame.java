ddf93d62ee7ec3c986338f7f242cb95b12329c36
Alex Dome <alex.dome.2022@student.lhschools.org>
2019-03-19 14:15:39 -0400

    Finished bulk of the project, only have to do loops

--- src/NumberGame.java
+++ src/NumberGame.java
@@ -2,8 +2,30 @@ import java.util.Scanner;
 
 public class NumberGame {
 
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
+        boolean value = true;
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        int number;
         int pickedNumber;
+
+        pickedNumber = (int) (Math.random() * 100);
+
+        System.out.println("Please try to guess my number out of 100.");
+        number = keyboard.nextInt();
+
+        while (value) {
+            if (number > pickedNumber) {
+                System.out.println("Sorry, " +number+ " is too high. Gimme another one.");
+                number = keyboard.nextInt();
+            }
+            if (number < pickedNumber) {
+                System.out.println("Sorry, " +number+ " is too low. Gimme another one.");
+                number = keyboard.nextInt();
+            }
+            if(number == pickedNumber);
+                value = false;
+        }
     }
 }
